e67c5562c6e7c96e0b690b2dd21c55e183e7648b
BekaKubla <bekakubla@gmail.com>
2026-03-14 14:33:33 +0400

    Enhance product categorization and UI in Products.razor
    Significantly modified the `Products.razor` file to improve UI components, layout, and search functionality. Redesigned the toast notification for better visibility.
    
    Expanded seed data in `ProductCategoryConfiguration.cs` to include more specific categories for spinning, carp, feeder, and floating subcategories.
    
    Updated `SubCategoryConfiguration.cs` to add additional subcategories for comprehensive product categorization.
    
    Modified `FishCoinDbContextModelSnapshot.cs` to align the database schema with the new product category structure.
    
    Created migration file `20260314102940_AddSubCategorySeedData.cs` to insert new categories and subcategories into the database, along with a supporting `Designer.cs` file.

--- FishCoinBlazorApp/Data/Configurations/ProductCategoryConfiguration.cs
+++ FishCoinBlazorApp/Data/Configurations/ProductCategoryConfiguration.cs
@@ -20,11 +20,51 @@ namespace FishCoinBlazorApp.Data.Configurations
             builder.HasOne(x => x.SubCategory).WithMany(x => x.ProductCategories).HasForeignKey(x => x.SubCategoryId);
 
             builder.HasData(
-                new ProductCategory { Id = 1, ProductCategoryName = "ჯოხები", SubCategoryId = 1 },
-                new ProductCategory { Id = 2, ProductCategoryName = "კოჭები", SubCategoryId = 1 },
-                new ProductCategory { Id = 3, ProductCategoryName = "სატყუარები", SubCategoryId = 1 },
-                new ProductCategory { Id = 4, ProductCategoryName = "აქსესუარები", SubCategoryId = 1 }
-            );
+
+            #region Spinning (SubCategoryId = 1)
+                new ProductCategory { Id = 1, ProductCategoryName = "ჯოხი", SubCategoryId = 1 },
+                new ProductCategory { Id = 2, ProductCategoryName = "კოჭა", SubCategoryId = 1 },
+                new ProductCategory { Id = 3, ProductCategoryName = "ნემსკავი", SubCategoryId = 1 },
+                new ProductCategory { Id = 4, ProductCategoryName = "ძუა/წნული/ფლურო", SubCategoryId = 1 },
+                new ProductCategory { Id = 5, ProductCategoryName = "ვობლერი", SubCategoryId = 1 },
+                new ProductCategory { Id = 6, ProductCategoryName = "ტრიალა", SubCategoryId = 1 },
+                new ProductCategory { Id = 7, ProductCategoryName = "ყანყალა", SubCategoryId = 1 },
+                new ProductCategory { Id = 8, ProductCategoryName = "ჯიგთავი", SubCategoryId = 1 },
+                new ProductCategory { Id = 9, ProductCategoryName = "სილიკონი", SubCategoryId = 1 },
+                new ProductCategory { Id = 10, ProductCategoryName = "აქსესუარები", SubCategoryId = 1 },
+            #endregion
+
+            #region Carp (SubCategoryId = 2)
+                new ProductCategory { Id = 11, ProductCategoryName = "ჯოხი", SubCategoryId = 2 },
+                new ProductCategory { Id = 12, ProductCategoryName = "კოჭა", SubCategoryId = 2 },
+                new ProductCategory { Id = 13, ProductCategoryName = "ნემსკავი/რიგები", SubCategoryId = 2 },
+                new ProductCategory { Id = 14, ProductCategoryName = "ძუა/ლიდკორი", SubCategoryId = 2 },
+                new ProductCategory { Id = 15, ProductCategoryName = "სვიველი/PVA/სიმძიმე", SubCategoryId = 2 },
+                new ProductCategory { Id = 16, ProductCategoryName = "სატყუარა/ბოილი/პელეტსი", SubCategoryId = 2 },
+                new ProductCategory { Id = 17, ProductCategoryName = "პოდი/სიგნალიზატორი", SubCategoryId = 2 },
+                new ProductCategory { Id = 18, ProductCategoryName = "ინვენტარი (სკამი/ბადე)", SubCategoryId = 2 },
+                new ProductCategory { Id = 19, ProductCategoryName = "აქსესუარები", SubCategoryId = 2 },
+            #endregion
+
+            #region Feeder (SubCategoryId = 3)
+                new ProductCategory { Id = 20, ProductCategoryName = "ჯოხი", SubCategoryId = 3 },
+                new ProductCategory { Id = 21, ProductCategoryName = "კოჭა", SubCategoryId = 3 },
+                new ProductCategory { Id = 22, ProductCategoryName = "ნემსკავი/სადავე", SubCategoryId = 3 },
+                new ProductCategory { Id = 23, ProductCategoryName = "ძუა/წნული", SubCategoryId = 3 },
+                new ProductCategory { Id = 24, ProductCategoryName = "დასაკვები/დანამატები", SubCategoryId = 3 },
+                new ProductCategory { Id = 25, ProductCategoryName = "საკვებურა", SubCategoryId = 3 },
+                new ProductCategory { Id = 26, ProductCategoryName = "აღჭურვილობა (სკამი/ბადე)", SubCategoryId = 3 },
+                new ProductCategory { Id = 27, ProductCategoryName = "აქსესუარები", SubCategoryId = 3 },
+            #endregion
+
+            #region Floating (SubCategoryId = 4)
+                new ProductCategory { Id = 28, ProductCategoryName = "ჯოხი", SubCategoryId = 4 },
+                new ProductCategory { Id = 29, ProductCategoryName = "კოჭა", SubCategoryId = 4 },
+                new ProductCategory { Id = 30, ProductCategoryName = "ტივტივა", SubCategoryId = 4 },
+                new ProductCategory { Id = 31, ProductCategoryName = "ნემსკავი/ძუა/სიმძიმე", SubCategoryId = 4 },
+                new ProductCategory { Id = 32, ProductCategoryName = "აქსესუარები", SubCategoryId = 4 }
+                #endregion
+);
         }
     }
 }
---
+++ FishCoinBlazorApp/Migrations/20260314102940_AddSubCategorySeedData.cs
@@ -0,0 +1,283 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FishCoinBlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubCategorySeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ProductCategoryName",
+                value: "ჯოხი");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ProductCategoryName",
+                value: "კოჭა");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProductCategoryName",
+                value: "ნემსკავი");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ProductCategoryName",
+                value: "ძუა/წნული/ფლურო");
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "ProductCategoryName", "SubCategoryId" },
+                values: new object[,]
+                {
+                    { 5, "ვობლერი", 1 },
+                    { 6, "ტრიალა", 1 },
+                    { 7, "ყანყალა", 1 },
+                    { 8, "ჯიგთავი", 1 },
+                    { 9, "სილიკონი", 1 },
+                    { 10, "აქსესუარები", 1 },
+                    { 11, "ჯოხი", 2 },
+                    { 12, "კოჭა", 2 },
+                    { 13, "ნემსკავი/რიგები", 2 },
+                    { 14, "ძუა/ლიდკორი", 2 },
+                    { 15, "სვიველი/PVA/სიმძიმე", 2 },
+                    { 16, "სატყუარა/ბოილი/პელეტსი", 2 },
+                    { 17, "პოდი/სიგნალიზატორი", 2 },
+                    { 18, "ინვენტარი (სკამი/ბადე)", 2 },
+                    { 19, "აქსესუარები", 2 },
+                    { 20, "ჯოხი", 3 },
+                    { 21, "კოჭა", 3 },
+                    { 22, "ნემსკავი/სადავე", 3 },
+                    { 23, "ძუა/წნული", 3 },
+                    { 24, "დასაკვები/დანამატები", 3 },
+                    { 25, "საკვებურა", 3 },
+                    { 26, "აღჭურვილობა (სკამი/ბადე)", 3 },
+                    { 27, "აქსესუარები", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SubCategories",
+                columns: new[] { "Id", "CategoryId", "SubCategoryName" },
+                values: new object[,]
+                {
+                    { 4, 1, "ტივტივა" },
+                    { 5, 1, "აქსესუარი" },
+                    { 6, 1, "საჩუქარი" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "ProductCategoryName", "SubCategoryId" },
+                values: new object[,]
+                {
+                    { 28, "ჯოხი", 4 },
+                    { 29, "კოჭა", 4 },
+                    { 30, "ტივტივა", 4 },
+                    { 31, "ნემსკავი/ძუა/სიმძიმე", 4 },
+                    { 32, "აქსესუარები", 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "SubCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "SubCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "SubCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ProductCategoryName",
+                value: "ჯოხები");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ProductCategoryName",
+                value: "კოჭები");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProductCategoryName",
+                value: "სატყუარები");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ProductCategoryName",
+                value: "აქსესუარები");
+        }
+    }
+}
