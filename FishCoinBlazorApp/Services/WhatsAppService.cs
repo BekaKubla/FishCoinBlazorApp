8a453d0cba03befc6820bc978780171937230ddc
BekaKubla <bekakubla@gmail.com>
2026-03-16 16:59:15 +0400

    Add WhatsApp integration and enhance order success UI
    Updated `OrderSuccess.razor` to inject `WhatsAppService`, improve points display, and add a settlement button. Changed navigation to redirect to the products page.
    
    In `WhatsAppService.cs`, removed unused directive and added `GetSettlementOrderUrl` method for order confirmation messages with improved formatting.

--- FishCoinBlazorApp/Services/WhatsAppService.cs
+++ FishCoinBlazorApp/Services/WhatsAppService.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using FishCoinBlazorApp.Entites.Product;
+using System.Web;
 
 namespace FishCoinBlazorApp.Services
 {
@@ -10,13 +11,35 @@ namespace FishCoinBlazorApp.Services
         public string GetProductOrderUrl(string productName, decimal price, string? productNumber, int? quantity)
         {
             // ტექსტი ავაწყოთ ერთ ცვლადად
-            string text = $"გამარჯობა, მაინტერესებს ეს პროდუქტი ({productNumber}):\n" +
-                          $"დასახელება: {productName}\n" +
-                          $"საცალო ფასი: {price} ლარი\n" +
-                          $"რაოდენობა: {quantity} ცალი\n" +
-                          $"ჯამში: {price * quantity} ლარი\n";
+            string text = $"*🔍 ინფორმაცია პროდუქტზე*\n\n" +
+              $"გამარჯობა, მაინტერესებს მოცემული პროდუქტი:\n\n" +
+              $"*🔢 პროდუქტის #:* {productNumber}\n" +
+              $"*📦 დასახელება:* {productName}\n" +
+              $"*🏷️ საცალო ფასი:* {price} ლარი\n" +
+              $"*🔢 რაოდენობა:* {quantity} ცალი\n" +
+              $"--------------------------\n" +
+              $"*💰 ჯამური ღირებულება:* {price * quantity} ლარი\n\n" +
+              $"_გთხოვთ, დამიდასტუროთ არის თუ არა მარაგში._";
 
             // გამოიყენე HttpUtility.UrlEncode უფრო საიმედოა დაშორებებისთვის
+            string encodedText = HttpUtility.UrlEncode(text);
+
+            // საბოლოო URL
+            return $"https://api.whatsapp.com/send?phone={_myPhoneNumber}&text={encodedText}";
+        }
+
+        public string GetSettlementOrderUrl(Order order)
+        {
+            // ტექსტი ავაწყოთ ერთ ცვლადად
+            string text = $"*💳 შეკვეთის დადასტურება და გადარიცხვა*\n\n" +
+              $"გამარჯობა, მსურს ამ შეკვეთის საფასურის *გადარიცხვით* გადახდა:\n\n" +
+              $"*📦 შეკვეთის #:* {order.OrderNumber}\n" +
+              $"*💰 გადასარიცხი თანხა:* {order.TotalAmountGEL} ლარი\n" +
+              $"--------------------------\n\n" +
+              $"*👤 მყიდველი:* {order.FirstNameAndLastName}\n" +
+              $"*📞 ტელეფონი:* {order.PhoneNumber}\n\n" +
+              $"_გთხოვთ გამომიგზავნოთ რეკვიზიტები, რომ მოვახდინო ანგარიშსწორება._";
+
             string encodedText = HttpUtility.UrlEncode(text);
 
             // საბოლოო URL
